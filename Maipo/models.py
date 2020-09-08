6ed2db3a70c7ac305afc892d0b1835bcd02b6c90
Harold <har.oyarzun@alumnos.duoc.cl>
2020-09-08 18:55:25 -0300

    Primera subida de la pagina

---
+++ Maipo/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Productor (models.Model):
+    nombre = models.CharField(max_length=60)
+    correo = models.CharField(max_length=100)
+    rut = models.CharField(max_length=10)
+    edad = models.CharField(max_length=3)
+    telofono = models.CharField(max_length=10)
+    genero = models.CharField(max_length=10)
+    direccion = models.CharField(max_length=10)
+    nacionalidad = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nombre
