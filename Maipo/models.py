c02266f0e359a415c4b07b354ce71581b420d18b
Phillippe93 <arancibia.1993@live.com>
2020-10-04 16:34:49 -0300

    Creacion de algunas clases

--- Maipo/models.py
+++ Maipo/models.py
@@ -13,3 +13,48 @@ class Productor (models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Cliente_Interno (models.Model):
+    nombre: models.CharField(max_length=60)
+    apellido: models.CharField(max_length=60)
+    correo: models.CharField(max_length=40)
+    rut: models.CharField(max_length=10)
+    region: models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.nombre
+
+class Cliente_Externo (models.Model):
+    nombre: models.CharField(max_length=60)
+    apellido: models.CharField(max_length=60)
+    correo: models.CharField(max_length=40)
+    rut: models.CharField(max_length=10)
+    pais: models.CharField(max_length=15)
+    region: models.CharField(max_length=30)
+    ciudad: models.CharField(max_length=40)
+    codigoPostal: models.IntegerField()
+    
+    def __str__(self):
+        return self.nombre
+
+class Transportista (models.Model):
+    nombre: models.CharField(max_length=60)
+    apellido: models.CharField(max_length=60)
+    rut: models.CharField(max_length=10)
+    tipoLicencia: models.CharField(max_length=1)
+    numeroContacto: models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+class Producto (models.Model):
+    nombreProducto = models.CharField(max_length=20)
+    cantidad = models.IntegerField()
+    precio = models.IntegerField()
+    descripcion = models.TextField()
+    dueñoProducto = models.ForeignKey(Productor, on_delete=models.CASCADE)
+    fechaPublicacion = models.DateTimeField()
+
+    def __str__(self):
+        return self.nombreProducto
+
