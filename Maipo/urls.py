5c5f194d7189021a4156caa098e15f093df60026
Harold <har.oyarzun@alumnos.duoc.cl>
2020-10-13 19:20:54 -0300

    login funcional

--- Maipo/urls.py
+++ Maipo/urls.py
@@ -6,10 +6,7 @@ from .views import mostrarProducto,añadirProducto
 urlpatterns = [
     path('',views.index,name='index'),
     path('galeria/',views.galeria,name='galeria'),
-    path('register/', views.register, name='formulario'),
-    path('login/', views.login, name='login'),
     path('lista_productos/', views.mostrarProducto, name='lista_productos'),
-    path('logout/', views.logout, name='salir'),
     path('añadir_producto/',views.añadirProducto, name='añadir_producto'),
     
 ]
