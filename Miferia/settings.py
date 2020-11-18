94c24a7b8f15ca1edfa73aaae72d80a7a8436764
Harold <har.oyarzun@alumnos.duoc.cl>
2020-11-18 13:16:59 -0300

    heroku

--- Miferia/settings.py
+++ Miferia/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
@@ -130,5 +131,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #AUTH_USER_MODEL = 'Maipo.User'
