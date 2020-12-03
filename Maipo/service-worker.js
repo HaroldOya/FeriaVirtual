688bebc741681b4397f6968a0a821a788307c102
Daniel Tapia <danasayake@gmail.com>
2020-12-03 17:21:33 -0300

    BugFix Service Worker

--- Maipo/service-worker.js
+++ Maipo/service-worker.js
@@ -1,81 +1,51 @@
-var cacheName = 'FeriaCache';
-var filesToCache = [
-    '/',
-    '/js/app.js',
-    '/js/menu.js',
-    './service-worker.js',
-    '/js/formulario.js',
-    '/js/jquery-3.5.1.min.js',
-    '/js/letras.js',
-    '/js/mostrar.js',
-    '/js/formulario.js',
-    'añadir_producto.html',
-    'base.html',
-    'cart_detail.html',
-    'checkout.html',
-    'export.html',
-    'index.hmtl',
-    'lista_producto',
-    'mis_productos.html',
-    'modificar_producto.hmtl',
-    'subasta_apuesta.html',
-    'subasta.html',
-    'subastaRealizada.html',
-    'registration/login.html',
-    'registration/registrar_clienteEx.html',
-    'registration/registrar_clienteInt.html',
-    'registration/registro_productor.html',
-    'registration/registro_transportista',
-    'registros.html',
-    '/styles/estilos.css',
-    '/styles/font-awesome.css',
-    '/styles/font-awesome.min.css',
-    '/styles/fontello.css',
-    '/styles/galery.css',
-    '/styles/main.css',
-    '/styles/menu.css',
-    '/styles/responsivo.css',
-    '/styles/subasta.css',
-    '/img/correo50.png',
-    '/img/face50.png',
-  ];
-
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
-  );
+self.addEventListener("install", function(event) {
+  event.waitUntil(preLoad());
 });
 
-self.addEventListener('activate', function(e) {
-    console.log('[ServiceWorker] Activate');
+var preLoad = function(){
+  console.log("Installing web app");
+  return caches.open("offline").then(function(cache) {
+    console.log("caching index and important routes");
+    return cache.addAll(["/static/", "/static", "/", "/templates", "/templates/","/index.html"]);
+  });
+};
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(checkResponse(event.request).catch(function() {
+    return returnFromCache(event.request);
+  }));
+  event.waitUntil(addToCache(event.request));
 });
 
-self.addEventListener('activate', function(e) {
-    console.log('[ServiceWorker] Activate');
-    e.waitUntil(
-      caches.keys().then(function(keyList) {
-        return Promise.all(keyList.map(function(key) {
-          if (key !== cacheName) {
-            console.log('[ServiceWorker] Removing old cache', key);
-            return caches.delete(key);
-          }
-        }));
-      })
-    );
-    return self.clients.claim();
+var checkResponse = function(request){
+  return new Promise(function(fulfill, reject) {
+    fetch(request).then(function(response){
+      if(response.status !== 404) {
+        fulfill(response);
+      } else {
+        reject();
+      }
+    }, reject);
   });
+};
 
-  self.addEventListener('fetch', function(e) {
-    console.log('[ServiceWorker] Fetch', e.request.url);
-    e.respondWith(
-      caches.match(e.request).then(function(response) {
-        return response || fetch(e.request);
-      })
-    );
+var addToCache = function(request){
+  return caches.open("offline").then(function (cache) {
+    return fetch(request).then(function (response) {
+      console.log(response.url + " was cached");
+      return cache.put(request, response);
+    });
   });
-  
-  
+};
+
+var returnFromCache = function(request){
+  return caches.open("offline").then(function (cache) {
+    return cache.match(request).then(function (matching) {
+     if(!matching || matching.status == 404) {
+       return cache.match("offline.html");
+     } else {
+       return matching;
+     }
+    });
+  });
+};
